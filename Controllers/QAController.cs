fc94017f423d5b012cd789b13da07c7bd0efcb35
Hiep Nguyen Thanh <thanhhiep77777@gmail.com>
2021-12-11 21:34:01 +0700

    added controllers

---
+++ Controllers/QAController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HMT_StationeryMnt.Controllers
+{
+    public class QAController : Controller
+    {
+        // GET: QA
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/StationeryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HMT_StationeryMnt.Controllers
+{
+    public class StationeryController : Controller
+    {
+        // GET: Stationeries
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
