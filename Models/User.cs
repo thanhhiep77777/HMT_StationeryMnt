e1a87b011e60884fc5e1605703d0b1dbcc2aa0da
Hiep Nguyen Thanh <thanhhiep77777@gmail.com>
2021-12-12 19:59:42 +0700

    modified entity

--- Models/User.cs
+++ Models/User.cs
@@ -28,6 +28,7 @@ namespace HMT_StationeryMnt.Models
         public Nullable<int> Role { get; set; }
         public string Email { get; set; }
         public Nullable<int> Status { get; set; }
+        public string Password { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Request> Requests { get; set; }
